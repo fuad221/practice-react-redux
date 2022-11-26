a9a3ae76c2358774a39dbff0c3af7908fdc0f6da
Fuad2211 <kontakt-de@outlook.com>
2022-11-26 19:33:11 +0100

    i prepared Header component to work with Redux

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+
+
+const Header = () => {
+
+    return (
+        <header className={classes.header}>
+            <h1>Redux Auth</h1>
+            {isAuth && (
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='/'>My Products</a>
+                        </li>
+                        <li>
+                            <a href='/'>My Sales</a>
+                        </li>
+                        <li>
+                            <button onClick={logoutHandler}>Logout</button>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </header>
+    );
+};
+
+export default Header;
