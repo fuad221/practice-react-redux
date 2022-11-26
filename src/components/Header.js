6d88a0057c360b4c0a4d4bb1b817c8dcbc26852c
Fuad2211 <kontakt-de@outlook.com>
2022-11-26 20:24:40 +0100

    refactor the project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
+import { useSelector, useDispatch } from 'react-redux';
 
+import classes from './Header.module.css';
+import { authActions } from '../store/auth';
 
 const Header = () => {
+    const dispatch = useDispatch();
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+    const logoutHandler = () => {
+        dispatch(authActions.logout());
+    };
 
     return (
         <header className={classes.header}>
